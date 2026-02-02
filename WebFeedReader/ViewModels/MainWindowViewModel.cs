cdcb99b4acf2ae7940cf8ac81a679511e6dc3e7b
UmsrAkne <sibata486@gmail.com>
2026-02-02 12:09:05 +0900

    feat: Add IsLoading property and asynchronous Initialize method to MainWindowViewModel
    - Introduce `IsLoading` property with private setter
    - Add `InitializeAsync` method for future asynchronous initialization tasks

--- WebFeedReader/ViewModels/MainWindowViewModel.cs
+++ WebFeedReader/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using System.Threading.Tasks;
+using Prism.Mvvm;
 using WebFeedReader.Utils;
 
 namespace WebFeedReader.ViewModels;
@@ -7,6 +8,7 @@ public class MainWindowViewModel : BindableBase
 {
     private readonly AppVersionInfo appVersionInfo = new ();
     private readonly AppSettings appSettings;
+    private bool isLoading;
 
     public MainWindowViewModel()
     {
@@ -18,4 +20,25 @@ public class MainWindowViewModel : BindableBase
     }
 
     public string Title => appVersionInfo.Title;
+
+    public bool IsLoading { get => isLoading; private set => SetProperty(ref isLoading, value); }
+
+    public async Task InitializeAsync()
+    {
+        IsLoading = true;
+
+        try
+        {
+            // ダミー処理
+            await Task.CompletedTask;
+
+            // var json = await apiClient.GetFeedsAsync();
+            // var items = FeedItemFactory.FromJson(json);
+            // FeedItems = new ObservableCollection<FeedItem>(items);
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
 }
