615f5660e1f44efcb34f6d5ebaef446dcbd8551b
UmsrAkne <sibata486@gmail.com>
2026-02-02 10:49:32 +0900

    feat: Add NgWord model to represent restricted words with timestamps

---
+++ WebFeedReader/Models/NgWord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebFeedReader.Models
+{
+    public class NgWord
+    {
+        public int Id { get; set; }
+
+        public string Value { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
