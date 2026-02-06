8680ad6bf8796634ffc9733f726bd4e9cde30026
UmsrAkne <sibata486@gmail.com>
2026-02-06 18:50:02 +0900

    feat(ui/vm): add read status handling for feed items
    - Introduce `IsRead` property in `FeedItem` with support for change notification.
    - Update `SelectedItem` in `FeedListViewModel` to mark items as read upon selection and batch update the database.
    - Modify `MainWindow.xaml` to visually indicate read status with a dynamic border style.

--- WebFeedReader/ViewModels/FeedListViewModel.cs
+++ WebFeedReader/ViewModels/FeedListViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Prism.Mvvm;
 using WebFeedReader.Dbs;
@@ -10,6 +12,7 @@ namespace WebFeedReader.ViewModels
     public class FeedListViewModel : BindableBase
     {
         private readonly IFeedItemRepository repository;
+        private readonly List<FeedItem> readItems = new ();
         private ObservableCollection<FeedItem> items = new ();
         private FeedItem selectedItem;
 
@@ -20,12 +23,29 @@ namespace WebFeedReader.ViewModels
 
         public ObservableCollection<FeedItem> Items { get => items; private set => SetProperty(ref items, value); }
 
-        public FeedItem SelectedItem { get => selectedItem; set => SetProperty(ref selectedItem, value); }
+        public FeedItem SelectedItem
+        {
+            get => selectedItem;
+            set
+            {
+                if (value != null)
+                {
+                    value.IsRead = true;
+                    readItems.Add(value);
+                }
+
+                SetProperty(ref selectedItem, value);
+            }
+        }
 
         public async Task UpdateItemsAsync(FeedSource source)
         {
             var list = await repository.GetBySourceIdAsync(source.Id);
             Items = new ObservableCollection<FeedItem>(list);
+
+            await repository.MarkAsReadAsync(readItems.Select(i => i.Key));
+            readItems.Clear();
+
             System.Console.WriteLine($"UpdateItemsAsync: {source.Name}");
         }
     }
