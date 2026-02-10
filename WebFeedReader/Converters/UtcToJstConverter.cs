41f8e8aa34b2e70ea8121a7ac91d9987ac969b42
UmsrAkne <sibata486@gmail.com>
2026-02-10 21:18:08 +0900

    feat(converters): add UtcToJstConverter for date formatting in feed items
    - Introduce `UtcToJstConverter` to convert UTC timestamps to JST for display.
    - Update `MainWindow.xaml` to use the new converter for published date binding.

---
+++ WebFeedReader/Converters/UtcToJstConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WebFeedReader.Converters
+{
+    public sealed class UtcToJstConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not DateTimeOffset dto)
+            {
+                return Binding.DoNothing;
+            }
+
+            // DateTimeOffset.ToLocalTime() は OS のタイムゾーンを使う
+            var local = dto.ToLocalTime();
+
+            return local.ToString("yyyy/MM/dd HH:mm", culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
