f292731a33fb2a15df0312a02f2c6af9d00f34c3
UmsrAkne <sibata486@gmail.com>
2026-02-10 21:27:23 +0900

    feat(ui/vm): add SettingPageViewModel and SettingPage skeleton
    - Introduce `SettingPageViewModel` to manage settings-related logic.
    - Add `SettingPage.xaml` and its code-behind file for UI representation.
    - Register `SettingPageViewModel` in the IoC container.
    - Update `MainWindowViewModel` to include `SettingPageViewModel`.

---
+++ WebFeedReader/ViewModels/SettingPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mvvm;
+using WebFeedReader.Utils;
+
+namespace WebFeedReader.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class SettingPageViewModel : BindableBase
+    {
+        private readonly AppSettings appSettings;
+
+        public SettingPageViewModel(AppSettings appSettings)
+        {
+            this.appSettings = appSettings;
+        }
+    }
+}
