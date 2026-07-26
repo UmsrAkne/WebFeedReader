cdd2465b6743d004461072ea24665e65abd08d52
UmsrAkne <sibata486@gmail.com>
2026-07-26 19:50:03 +0900

    feat(vm): add NG-word sync to MainWindowViewModel initialization
    - Injected NgWordService into MainWindowViewModel.
    - Added NG-word retrieval and syncing during initialization.

--- WebFeedReader/ViewModels/MainWindowViewModel.cs
+++ WebFeedReader/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Input;
 using Prism.Mvvm;
@@ -22,6 +23,7 @@ public class MainWindowViewModel : BindableBase, IScrollResettable
     private readonly IFeedItemRepository feedItemRepository;
     private bool isLoading;
     private AsyncRelayCommand initializeCommand;
+    private readonly NgWordService ngWordService;
 
     public MainWindowViewModel()
     {
@@ -48,7 +50,8 @@ public class MainWindowViewModel : BindableBase, IScrollResettable
         NgListPageViewModel ngListPageViewModel,
         FeedSourceCreatePageViewModel feedSourceCreatePageViewModel,
         SettingPageViewModel settingPageViewModel,
-        FeedSourceListViewModel feedListVm)
+        FeedSourceListViewModel feedListVm,
+        NgWordService ngWordService)
     {
         this.appSettings = appSettings;
         this.feedSourceRepository = feedSourceRepository;
@@ -60,6 +63,8 @@ public class MainWindowViewModel : BindableBase, IScrollResettable
         FeedSourceCreatePageViewModel = feedSourceCreatePageViewModel;
         SettingPageViewModel = settingPageViewModel;
         FeedSourceListViewModel = feedListVm;
+
+        this.ngWordService = ngWordService;
 
         FeedSourceListViewModel.SelectedItemChanged += async source =>
         {
@@ -97,6 +102,16 @@ public class MainWindowViewModel : BindableBase, IScrollResettable
         IsLoading = true;
         try
         {
+            var ngWords = await ngWordService.GetAllNgWordsAsync();
+            var latestNgWordVersion = 0;
+            var ngWordList = ngWords.ToList();
+            if (ngWordList.Any())
+            {
+                latestNgWordVersion = ngWordList.Max(w => w.CreatedAt);
+            }
+
+            await ngWordService.SyncNgWordsAsync(latestNgWordVersion);
+
             await SyncFeedsAsync(appSettings.LastFeedsUpdate);
 
             var sources = await feedSourceRepository.GetAllAsync();
