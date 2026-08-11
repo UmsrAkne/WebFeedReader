4269750e61cfc5a076e10868662df82e8053d304
UmsrAkne <sibata486@gmail.com>
2026-08-11 09:44:56 +0900

    feat(ui): replace default toggle buttons with `CustomDesignToggleButton`
    - Introduced `CustomDesignToggleButton` component with customizable styles and animations.
    - Updated `MainWindow.xaml` to use the new component for "Filter (unread)" and "Newest first" options.
    - Added new dependency properties (`IsChecked`, `Label`, `Command`) in `CustomDesignToggleButton` to support binding.

---
+++ WebFeedReader/Controls/CustomDesignToggleButton.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WebFeedReader.Controls
+{
+    public partial class CustomDesignToggleButton
+    {
+        public readonly static DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register(
+                nameof(IsChecked),
+                typeof(bool?),
+                typeof(CustomDesignToggleButton),
+                new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public readonly static DependencyProperty LabelProperty =
+            DependencyProperty.Register(
+                nameof(Label),
+                typeof(string),
+                typeof(CustomDesignToggleButton),
+                new PropertyMetadata("Default Label"));
+
+        public readonly static DependencyProperty CommandProperty =
+            DependencyProperty.Register(
+                nameof(Command),
+                typeof(ICommand),
+                typeof(CustomDesignToggleButton));
+
+        public CustomDesignToggleButton()
+        {
+            InitializeComponent();
+        }
+
+        public bool? IsChecked { get => (bool?)GetValue(IsCheckedProperty); set => SetValue(IsCheckedProperty, value); }
+
+        public string Label { get => (string)GetValue(LabelProperty); set => SetValue(LabelProperty, value); }
+
+        public ICommand Command { get => (ICommand)GetValue(CommandProperty); set => SetValue(CommandProperty, value); }
+    }
+}
